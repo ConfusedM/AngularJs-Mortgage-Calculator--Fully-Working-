354053427976b2909a144114846d3eb793421e11
ConfusedM <gerryong2808@gmail.com>
2019-01-18 15:07:06 +0800

    done with count

--- count.php
+++ count.php
@@ -8,29 +8,73 @@ $yr4 = $_POST["int4"];
 $yr5 = $_POST["int5"];
 $yrs = $_POST["yrs"];
 
+
 function monrate($x){
 
 	return $x/1200;
 
 }
 
-function powerof($monrate, $periodleft){
+function countinterest($pri,$int){
 
+	return round(monrate($int) * $pri,2) ;
+
+}
+
+function powerof($monrate, $periodleft){
 
 	return pow(1 + $monrate,$periodleft);
 
 }
 
+function mthpay($pri, $yr1, $yrs){
 
-function countmonth1($pri, $yr1, $yrs){
-
-
-	return ($pri * monrate($yr1) * powerof(monrate($yr1), $yrs)) / (powerof(monrate($yr1), $yrs) - 1);
+	return round(($pri * monrate($yr1) * powerof(monrate($yr1), $yrs)) / (powerof(monrate($yr1), $yrs) - 1),2);
 
 }
 
+function buildtable($pri, $yr1, $yr2, $yr3, $yr4, $yr5, $yrs){
 
-echo countmonth1($pri, $yr1, $yrs*12);
+	$ppaid = 0;
+
+	for($x=1; $x <= 12; $x++){
+
+		echo "<tr>";
+
+			//month
+			echo "<td>" . $x . "</td>";
+
+			//payment
+			echo "<td>" . mthpay($pri, $yr1, ($yrs*12)-($x-1)) . "</td>";
+
+			//beginning principal
+			echo "<td>" . $pri . "</td>";
+
+			//interest
+			echo "<td>" . countinterest($pri,$yr1) . "</td>";
+
+			$ppaid = $ppaid + mthpay($pri, $yr1, ($yrs*12)-($x-1)) - countinterest($pri,$yr1);
+
+			//principal paid
+			echo "<td>" . $ppaid . "</td>";
+
+			$pri = $pri - $ppaid;
+
+			//final principal
+			echo "<td>" . $pri . "</td>";
+
+		echo "</tr>";
+
+
+	}
+
+
+}
+
+echo mthpay($pri, $yr1, $yrs*12);
+
+
+
 
 ?>
 
@@ -51,6 +95,12 @@ echo countmonth1($pri, $yr1, $yrs*12);
 			<th>Principal Paid</th>
 			<th>Final Principal</th>
 		</tr>
+
+		<?php
+
+			buildtable($pri, $yr1, $yr2, $yr3, $yr4, $yr5, $yrs);
+
+		?>
 	</table>
 
 </body>
